295aa15ffe134ffd9a57973e80c5e6c902a24f49
Graysland NL <graysland.nl@gmail.com>
2025-04-18 18:22:53 -0230

    Update index.js

--- admin/index.js
+++ admin/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const basicAuth = require('express-basic-auth');
 const schedule = require('./schedule');
+const path = require('path');
 
 const app = express();
 const port = process.env.ADMIN_PORT || 8001;
@@ -14,6 +15,14 @@ const adminAuth = basicAuth({
     users: { [process.env.ICECAST_ADMIN || 'admin']: process.env.ICECAST_PASSWORD || 'hackme' },
     challenge: true,
     realm: 'Icecast Admin'
+});
+
+// Serve static files
+app.use(express.static(path.join(__dirname)));
+
+// Admin interface route
+app.get('/admin', adminAuth, (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Schedule routes
